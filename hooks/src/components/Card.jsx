d25c2b125b7993798ce5bd30068cc9b2d204b27d
ijassm <ijass.ocean@gmail.com>
2025-04-23 16:16:54 +0530

    feat: add Card and CardList components for displaying user data

---
+++ hooks/src/components/Card.jsx
@@ -0,0 +1,9 @@
+export function Card({ title, userId, completed }) {
+  return (
+    <div className="border-2 border-amber-500 my-2 p-2">
+      <h1>UserId : {userId}</h1>
+      <h1>Title : {title}</h1>
+      <h1>Completed : {completed.toString()}</h1>
+    </div>
+  );
+}
