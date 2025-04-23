336f960a218b7403f942e79077d2bfb45cc0df69
ijassm <ijass.ocean@gmail.com>
2025-04-23 16:19:29 +0530

    feat: add Loader component for displaying loading state

---
+++ hooks/src/components/ui/Loader.jsx
@@ -0,0 +1,9 @@
+export function Loader({ loading }) {
+  if (loading) {
+    return (
+      <main className="flex flex-col items-center justify-center p-4 bg-gray-100 text-gray-900">
+        <h1 className="my-4 text-6xl">Loading...</h1>
+      </main>
+    );
+  }
+}
